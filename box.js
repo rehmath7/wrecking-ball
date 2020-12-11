1ef893ea2bd0b6b5e7588795d33c120e61e76756
rehmath7 <72341136+rehmath7@users.noreply.github.com>
2020-12-11 20:21:04 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,31 @@
+class Box {
+    constructor(x,y,width,height) {
+      var options = {
+          restitution:1,
+          friction:1,
+          density:0.04
+
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      // fill("brown");
+     // rect(pos.x, pos.y, this.width, this.height);
+      push();
+      translate(this.body.position.x, this.body.position.y);
+      rotate(angle);
+      rectMode(CENTER);
+     strokeWeight(4);
+     stroke("green");
+     fill("white");
+     rect(0,0,this.width,this.height);
+      pop();
+    }
+
+    
+  };
